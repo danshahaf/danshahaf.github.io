e1f70ae565b29574c4519349d5452c1edf68597c
Shahaf Dan <dan.shachaf@gmail.com>
2019-08-22 15:53:36 -0700

    added sorting algorithms - not done yet

--- shahaf.js
+++ shahaf.js
@@ -10,10 +10,19 @@ var sp19g = ['A', 'B', 'A', 'A', 'A', 'B'];
 var su19c = ["Intro to American Government - Political Science", ""]
 var su19g = ['A', 'A', 'A'];
 
+var f19c = ["Assembly Language", "Intro to Psychology", "Dicrete Mathematical Structures", "Professional Communications", "C++ Data Structures"];
+var f19g = ["N/A", "N/A", "N/A", "N/A", "N/A"];
 function sortAlphabetically()
 {
-    var allClasses = su18c.concat(f18c, sp19c, su19c);
+    if(!($('.classInfo').is(':empty')))
+    {
+        document.getElementById(classInfo).innerHTML = ""; // clean everything from that div
+    }
+    
+
+    var allClasses = su18c.concat(f18c, sp19c, su19c, f19c);
     allClasses.sort();
+
     for(var i = 0; i < allClasses.length; i++)
     {
         var p = document.createElement("p");
@@ -22,7 +31,47 @@ function sortAlphabetically()
         document.getElementById("classInfo").appendChild(p);
     }
 }
-function sortByGrade()
+function orderByGrade()
+{
+    if(!($('.classInfo').is(':empty')))
+    {
+        document.getElementById(classInfo).innerHTML = ""; // clean everything from that div
+    }
+    var allClasses = su18c.concat(f18c, sp19c, su19c, f19g);
+    var allGrades = su18g.concat(f18g, sp19g,su19g, f19g);
+    for(var j = 0; j < allGrades.length; j++) //check for A grades
+    {
+        if(allGrades[j] == 'A')
+        {
+            var p = document.createElement("p");
+            var text = document.createTextNode(allClasses[j] + "  -  " + allGrades[j]);
+            p.appendChild(text);
+            document.getElementById("classInfo").appendChild(p);
+        }
+    }
+    for(var j = 0; j < allGrades.length; j++) //check for B grades
+    {
+        if(allGrades[j] == 'B')
+        {
+            var p = document.createElement("p");
+            var text = document.createTextNode(allClasses[j] + "  -  " + allGrades[j]);
+            p.appendChild(text);
+            document.getElementById("classInfo").appendChild(p);
+        }
+    }
+    for(var j = 0; j < allGrades.length; j++) //check for C grades
+    {
+        if(allGrades[j] == 'C')
+        {
+            var p = document.createElement("p");
+            var text = document.createTextNode(allClasses[j] + "  -  " + allGrades[j]);
+            p.appendChild(text);
+            document.getElementById("classInfo").appendChild(p);
+        }
+    }
+}
+
+function sortByTerm()
 {
 
 }
