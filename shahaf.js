d018ec32c21142635545e0b8066fa3077e4026c8
Shahaf Dan <dan.shachaf@gmail.com>
2019-08-21 22:55:27 -0700

    Made the js file for sorting education classes

---
+++ shahaf.js
@@ -0,0 +1,12 @@
+var f18c = ["Multivariable Calculues", "Thermodynamics and Optical Physics", "English 7", "Computing Fundamentals II", "Swimming", "Diving"];
+var f18g = ['A', 'B', 'A','A', 'A', 'A'];
+
+var su18c = ["English 1"];
+var su18g = ['A'];
+
+var sp19c = ["C++ Data Structures", "Electricity and Magnetism Physics", "Philosophy of Ethics", "Dive Team Practices Class", "Internship Experience", "Linear Algebda"];
+var sp19g = ['A', 'B', 'A', 'A', 'A', 'B'];
+
+var su19c = ["Intro to American Government - Political Science", ""]
+var su19g = ['A', 'A', 'A'];
+
